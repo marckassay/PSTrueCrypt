93fa9c87a57a73c70ca402fae01e4c9c30a0f72e
Marc Kassay <marckassay@gmail.com>
2017-08-07 17:01:23 -0400

    Updated Get-TrueCryptDismountParams.Tests

--- test/Get-TrueCryptDismountParams.Tests.ps1
+++ test/Get-TrueCryptDismountParams.Tests.ps1
@@ -1,10 +1,15 @@
+Import-Module $PSScriptRoot\..\src\PSTrueCrypt.CommandLine.psm1
+
 Describe "Get-TrueCryptDismountParams when called..." {
-    InModuleScope PSTrueCrypt {
-        It "Should return expected string" {
-            Get-TrueCryptDismountParams -Drive 'T' -Product 'TrueCrypt' | Should BeExactly '& TrueCrypt /dismount T /quit' 
-        }
-        It "Should return expected string" {
-            Get-TrueCryptDismountParams -Drive 'V' -Product 'VeraCrypt' | Should BeExactly '& VeraCrypt /dismount V /quit' 
+    Context "with valid data" {
+        InModuleScope PSTrueCrypt.CommandLine {
+            It "Should return expected string" {
+                Get-TrueCryptDismountParams -Drive 'T' -Product 'TrueCrypt' | Should BeExactly '& TrueCrypt /dismount T /quit' 
+            }
+            
+            It "Should return expected string" {
+                Get-TrueCryptDismountParams -Drive 'V' -Product 'VeraCrypt' | Should BeExactly '& VeraCrypt /dismount V /quit' 
+            }
         }
     }
 }
