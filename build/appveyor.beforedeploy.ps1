130d7b3a8ff3d748d33c5d5c1cf98d08dd2c2d25
Marc Kassay <marckassay@gmail.com>
2017-08-23 22:05:54 -0400

    squash me

--- build/appveyor.beforedeploy.ps1
+++ build/appveyor.beforedeploy.ps1
@@ -1,19 +1,19 @@
 New-Object psobject (Get-Content .\PSTrueCrypt.psd1 -Raw | Invoke-Expression) -OutVariable Manifest | Out-Null
 
-    Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse -File | ForEach-Object {
+$env:APPVEYOR_BUILD_FOLDER | Get-ChildItem -Recurse -File | ForEach-Object {
 
-        $SubdirectoryPath = ($_.FullName -split "PSTrueCrypt\\").Get(1)
+    $SubdirectoryPath = ($_.FullName -split "PSTrueCrypt\\").Get(1)
 
-        if($Manifest.FileList -contains $SubdirectoryPath) {
+    if($Manifest.FileList -contains $SubdirectoryPath) {
 
-            Join-Path ("$env:APPVEYOR_BUILD_FOLDER\Out") -ChildPath $SubdirectoryPath | Split-Path -Parent -OutVariable DestinationParent | Out-Null
+        Join-Path ("$env:APPVEYOR_BUILD_FOLDER\Out") -ChildPath $SubdirectoryPath | Split-Path -Parent -OutVariable DestinationParent | Out-Null
 
-            if((Test-Path $DestinationParent) -eq $False) {
-                $DirectoryInfo = New-Item $DestinationParent -ItemType Directory
-            } else {
-                $DirectoryInfo = Get-Item $DestinationParent
-            }
-
-            $_.FullName | Copy-Item -Destination $DirectoryInfo
+        if((Test-Path $DestinationParent) -eq $False) {
+            $DirectoryInfo = New-Item $DestinationParent -ItemType Directory
+        } else {
+            $DirectoryInfo = Get-Item $DestinationParent
         }
-      }
+
+        $_.FullName | Copy-Item -Destination $DirectoryInfo
+    }
+}
