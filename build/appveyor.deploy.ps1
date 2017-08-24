bb6255e308fce3fa7ef266114a1464d1fca27a03
Marc Kassay <marckassay@gmail.com>
2017-08-24 08:25:40 -0400

    squash me

--- build/appveyor.deploy.ps1
+++ build/appveyor.deploy.ps1
@@ -1,14 +1,15 @@
-New-Object psobject (Get-Content .\PSTrueCrypt.psd1 -Raw | Invoke-Expression) -OutVariable Manifest | Out-Null
+New-Object psobject (Get-Content $ENV:APPVEYOR_BUILD_FOLDER\PSTrueCrypt.psd1 -Raw | Invoke-Expression) -OutVariable Manifest | Out-Null
 
-New-Item ("$env:APPVEYOR_BUILD_FOLDER\Out") -ItemType Directory
+New-Item $ENV:OUT_DEPLOY -ItemType Directory
+Set-Location $ENV:OUT_DEPLOY
 
-Get-ChildItem -Recurse -File | ForEach-Object {
+Get-ChildItem -Path $ENV:APPVEYOR_BUILD_FOLDER -Recurse -File | ForEach-Object {
 
     $SubdirectoryPath = ($_.FullName -split "PSTrueCrypt\\").Get(1)
 
     if($Manifest.FileList -contains $SubdirectoryPath) {
 
-        Join-Path ("$env:APPVEYOR_BUILD_FOLDER\Out") -ChildPath $SubdirectoryPath | Split-Path -Parent -OutVariable DestinationParent | Out-Null
+        Join-Path $ENV:OUT_DEPLOY -ChildPath $SubdirectoryPath | Split-Path -Parent -OutVariable DestinationParent | Out-Null
 
         if((Test-Path $DestinationParent) -eq $False) {
             $DirectoryInfo = New-Item $DestinationParent -ItemType Directory
@@ -20,4 +21,4 @@ Get-ChildItem -Recurse -File | ForEach-Object {
     }
 }
 
-Publish-Module -Path ("$env:APPVEYOR_BUILD_FOLDER\Out") -NuGetApiKey $env:MyNuGetApiKey
+Publish-Module -Path $ENV:OUT_DEPLOY -NuGetApiKey $ENV:MY_NUGET_API_KEY
